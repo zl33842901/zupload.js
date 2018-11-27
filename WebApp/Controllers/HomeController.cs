70387dc72190acbefc8fa291458ec64b64437b32
zl33842901 <zhanglei33842901@qq.com>
2018-11-27 16:14:18 +0800

    第一版

---
+++ WebApp/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public void Upload()
+        {
+            foreach(var i in Request.Files)
+            {
+                //上传的文件
+            }
+            foreach(var i in Request.Form)
+            {
+                //其他参数
+            }
+        }
+    }
+}
